fb12ac915a13da4d89714cdb7c24bd3bd5f97724
kosimaru1997 <kosimaru1997@gmail.com>
2022-07-30 22:29:28 +0900

    正規表現チェック処理を追加

--- src/app/controllers/api/problems_controller.rb
+++ src/app/controllers/api/problems_controller.rb
@@ -1,13 +1,22 @@
 class Api::ProblemsController < ApplicationController
 
   def index
-    @problems = Problem.all.to_json(only: [:id, :title, :content])
-    render json: @problems
+    problems = Problem.all.to_json(only: [:id, :title, :content])
+    render json: problems
   end
 
   def show
-    @problem = Problem.find(params[:id]).to_json(except: [:created_at, :updated_at])
-    render json: @problem
+    @problem = Problem.includes(:problem_test_cases).find(params[:id])
+    render json: problem.to_json(include: [:problem_test_cases])
+  end
+
+  def check
+    regex = Regexp.compile(params[:regex])
+    test_cases = Problem.find(params[:id]).problem_test_cases
+    collect_cases = test_cases.filter(&:is_collect)
+    checked_cases = test_cases.filter { |test| regex.match?(test.test_case) }
+    is_collect_regex = collect_cases == checked_cases
+    render json: { is_collect_regex:, checked_cases: }
   end
 
 end
